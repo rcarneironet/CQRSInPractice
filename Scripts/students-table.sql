0d897892a7c2ed04ca61282dfb3c235f0b06f46c
Ray Carneiro <rcarneironet@gmail.com>
2019-07-15 11:10:18 -0300

    Included db scripts

---
+++ Scripts/students-table.sql
@@ -0,0 +1,23 @@
+USE [CQRSinPractice]
+GO
+
+/****** Object:  Table [dbo].[Students]    Script Date: 12/07/2019 21:54:01 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Students](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+	[Age] [int] NOT NULL,
+	[Address] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Students] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
